5016d2549fbce99add1bed205390ecaef7114d0c
brx-nbif <blondeau.nbif@mail.ru>
2025-02-05 13:11:36 +0100

    chore: display the token in the index page

--- src/components/pages/home/page.tsx
+++ src/components/pages/home/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "@/components/design/button";
+import { Route } from "@/routes/_authenticated/index";
 
 export default function PageComponent() {
-  return <Button />;
+  const { token } = Route.useRouteContext();
+
+  return (
+    <div className="text-center">
+      <code className="mb-3 inline-block">token - {token}</code>
+      <Button />
+    </div>
+  );
 }
