8da157fa7a3f8b5f137ff6c2af86b81cdc6146be
brx-nbif <blondeau.nbif@mail.ru>
2025-02-12 00:22:32 +0100

    refactor: change the template looking

--- src/components/design/button.tsx
+++ src/components/design/button.tsx
@@ -8,7 +8,7 @@ export const Button = () => {
       <button
         id="count"
         onClick={increment}
-        className="cursor-pointer rounded-md bg-black px-4 py-2 text-white"
+        className="cursor-pointer rounded-full bg-white px-4 py-2 font-semibold text-black"
       >
         count - {count}
       </button>
--- src/components/pages/home/page.tsx
+++ src/components/pages/home/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/design/button";
+import { MorphingText } from "@/components/design/morphing-text";
+
+const stack = [
+  "React",
+  "Vite",
+  "TanStack",
+  "TypeScript",
+  "TailwindCSS",
+  "Vitest",
+  "Docker",
+  "Husky",
+  "Commitlint",
+];
 
 export default function PageComponent() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-7xl font-bold uppercase">Brx Template</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6">
+      <MorphingText texts={stack} className="text-white" />
       <Button />
+
+      <div className="text-neutral-200">
+        start editing <code>src/components/home/page.tsx</code> to see changes
+      </div>
     </div>
   );
 }
